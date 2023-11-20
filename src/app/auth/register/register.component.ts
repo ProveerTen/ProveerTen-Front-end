9a06abd2fe5ff00a2f5ef59ca913620b0761dd28
Ethiem <juangalvis2291@gmail.com>
2023-11-20 17:04:36 -0500

    Register with animation, but without logic

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  showGrocerForm: boolean = true;
+
+  toggleForm(role: string){
+    if (role === 'grocer') {
+      this.showGrocerForm = true; // Mostrar siempre el formulario del tendero
+    } else {
+      this.showGrocerForm = false; // Cambiar solo al formulario del proveedor
+    }
+  }
 }
