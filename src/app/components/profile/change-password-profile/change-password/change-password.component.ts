964e35c91a8248c77524647fcee809fc8c818771
sofii11108 <sofia35025@gmail.com>
2024-02-09 11:28:27 -0500

    error arreglado del componente

--- src/app/components/profile/change-password-profile/change-password/change-password.component.ts
+++ src/app/components/profile/change-password-profile/change-password/change-password.component.ts
@@ -15,8 +15,8 @@ export class ChangePasswordComponent {
 
   form: FormGroup;
   data: any;
-  isGrocer:any;
-  constructor(public auth: AuthService, private fb: FormBuilder, private client: ClientService, private router:Router) {
+  isGrocer: any;
+  constructor(public auth: AuthService, private fb: FormBuilder, private client: ClientService, private router: Router) {
 
     this.form = this.fb.group({
       old_password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
@@ -26,46 +26,120 @@ export class ChangePasswordComponent {
   }
 
   onSubmit() {
-     
+
     this.auth.isCompany().subscribe(value => {
-      if(value) {
+      if (value) {
         return this.onSubmitCompany();
       }
-  })
+    })
     this.auth.isGrocer().subscribe(value => {
-      if(value) {
+      if (value) {
         return this.onSubmitGrocer();
       }
-  })
-}
+    })
+  }
 
   onSubmitCompany() {
 
-    this.data = {
-      old_password: this.form.value.old_password,
-      new_password: this.form.value.new_password,
-    }
+    if (this.form.valid && !this.form.pristine) {
 
-    this.client.postRequest(`${environment.url_logic}/password/changePassword/company`, this.data, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
-      next: (response: any) => {
-        console.log("response", response);
-        Swal.fire({
-          title: "Contraseña actualizada con éxito",
-          icon: "success"
-        });
-        this.router.navigate(['/profile', this.auth.getId()])        
-      },
-      error: (error: any) => {
-        console.log("error", error);
-        Swal.fire({
-          title: `${error.error.error}`,
-          icon: "error"
-        });
-      },
-      complete: () => {
-        console.log("complete");
+      this.data = {
+        old_password: this.form.value.old_password,
+        new_password: this.form.value.new_password,
       }
-    })
+
+      this.client.postRequest(`${environment.url_logic}/password/changePassword/company`, this.data, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
+        next: (response: any) => {
+          console.log("response", response);
+          Swal.fire({
+            title: "Contraseña actualizada con éxito",
+            icon: "success"
+          });
+          this.form.reset()
+          this.router.navigate(['/profile', this.auth.getId()])        
+        },
+        error: (error: any) => {
+          console.log("error", error);
+          Swal.fire({
+            title: `${error.error.error}`,
+            icon: "error"
+          });
+        },
+        complete: () => {
+          console.log("complete");
+        }
+      })
+    } else {
+      console.log("Datos no validos");
+
+    }
+  }
+
+
+  onSubmitGrocer() {
+
+    if (this.form.valid && !this.form.pristine) {
+
+      this.data = {
+        old_password: this.form.value.old_password,
+        new_password: this.form.value.new_password,
+      }
+
+      this.client.postRequest(`${environment.url_logic}/password/changePassword/grocer`, this.data, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
+        next: (response: any) => {
+          console.log("response", response);
+          Swal.fire({
+            title: "Contraseña actualizada con éxito",
+            icon: "success"
+          });
+          this.form.reset()
+          // this.router.navigate(['/profile', this.auth.getId()])        
+          this.router.navigate(['/update-profile', this.auth.getRole()]);
+        },
+        error: (error: any) => {
+          console.log("error", error);
+          Swal.fire({
+            title: `${error.error.error}`,
+            icon: "error"
+          });
+        },
+        complete: () => {
+          console.log("complete");
+        }
+      })
+    } else {
+      console.log("Datos no validos");
+
+    }
+  }
+
+  isvalid(nameField: string) {
+    // console.log("is valid", this.form.get(nameField)?.valid);
+    return this.form.get(nameField)?.valid;
+  }
+
+  confirmPass(valor?: string) {
+    let result;
+    const newPassword = this.form.get('new_password')?.value;
+
+    const confirmedPassword = this.form.get('new_password_confirmed')?.value;
+
+    // console.log("chat", newPassword === confirmedPassword);
+    // console.log(confirmedPassword)
+    // console.log(newPassword === confirmedPassword && confirmedPassword != '');
+
+    if (confirmedPassword != '') {
+      if (newPassword === confirmedPassword) {
+        result = true;
+      } else {
+        result = false
+      }
+    } else if (valor === '0') {
+      result = false
+    } else if (valor == '1') {
+      result = true
+    }
+    return result;
   }
 
   onSubmitProvider() {
@@ -82,7 +156,7 @@ export class ChangePasswordComponent {
           title: "Contraseña actualizada con éxito",
           icon: "success"
         });
-        this.router.navigate(['/profile', this.auth.getId()])        
+        this.router.navigate(['/profile', this.auth.getId()])
       },
       error: (error: any) => {
         console.log("error", error);
@@ -95,65 +169,5 @@ export class ChangePasswordComponent {
         console.log("complete");
       }
     })
-  }
-
-  onSubmitGrocer() {
-
-    this.data = {
-      old_password: this.form.value.old_password,
-      new_password: this.form.value.new_password,
-    }
-
-    this.client.postRequest(`${environment.url_logic}/password/changePassword/grocer`, this.data, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
-      next: (response: any) => {
-        console.log("response", response);
-        Swal.fire({
-          title: "Contraseña actualizada con éxito",
-          icon: "success"
-        });
-        // this.router.navigate(['/profile', this.auth.getId()])        
-        this.router.navigate(['/update-profile', this.auth.getRole()]);
-      },
-      error: (error: any) => {
-        console.log("error", error);
-        Swal.fire({
-          title: `${error.error.error}`,
-          icon: "error"
-        });
-      },
-      complete: () => {
-        console.log("complete");
-      }
-    })
-  }
-
-  isvalid(nameField: string) {
-    // console.log("is valid", this.form.get(nameField)?.valid);
-    return this.form.get(nameField)?.valid;
-  }
-
-  
-  confirmPass(valor?:string) {
-    let result;
-    const newPassword = this.form.get('new_password')?.value;
-
-    const confirmedPassword = this.form.get('new_password_confirmed')?.value;
-
-    // console.log("chat", newPassword === confirmedPassword);
-    // console.log(confirmedPassword)
-    // console.log(newPassword === confirmedPassword && confirmedPassword != '');
-
-    if (confirmedPassword != '') {
-      if (newPassword === confirmedPassword) {
-        result = true;
-      } else {
-        result = false
-      }
-    } else if(valor === '0') {
-      result = false
-    } else if(valor == '1') {
-      result = true
-    }
-    return result;
   }
 }
