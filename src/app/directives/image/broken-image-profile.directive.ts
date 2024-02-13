04699f5e8fad238c183beb216d1cb67faf958ce6
Jhon Stiven Arcila Herrera <110495088+JhoonHe@users.noreply.github.com>
2024-02-13 15:36:57 -0500

    Update directive photo

--- src/app/directives/image/broken-image-profile.directive.ts
+++ src/app/directives/image/broken-image-profile.directive.ts
@@ -13,7 +13,7 @@ export class BrokenImageProfileDirective {
   load_default_image() {
     const element = this.elementRef.nativeElement;
     if (this.imageType === 'photo') {
-      element.src = 'https://res.cloudinary.com/ddio1oisb/image/upload/v1707840717/erynrxk1jnpyegelq3ig.png';
+      element.src = 'https://res.cloudinary.com/ddio1oisb/image/upload/v1707856438/oxp0zsvbcvsfe67wmngr.png';
     } else if (this.imageType === 'cover') {
       element.src = 'https://res.cloudinary.com/ddio1oisb/image/upload/v1707797559/pngvkepjh9pewj1xkek0.jpg';
     }
