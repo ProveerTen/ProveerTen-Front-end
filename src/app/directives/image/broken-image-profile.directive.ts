54bb0e425a9fe1c3ee33ef00808afaf13f7bc0b6
Jhon Herrera <jjhonherrera113@gmail.com>
2024-02-13 11:26:59 -0500

    Update profiles

--- src/app/directives/image/broken-image-profile.directive.ts
+++ src/app/directives/image/broken-image-profile.directive.ts
@@ -13,7 +13,7 @@ export class BrokenImageProfileDirective {
   load_default_image() {
     const element = this.elementRef.nativeElement;
     if (this.imageType === 'photo') {
-      element.src = 'https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg';
+      element.src = 'https://res.cloudinary.com/ddio1oisb/image/upload/v1707840717/erynrxk1jnpyegelq3ig.png';
     } else if (this.imageType === 'cover') {
       element.src = 'https://res.cloudinary.com/ddio1oisb/image/upload/v1707797559/pngvkepjh9pewj1xkek0.jpg';
     }
