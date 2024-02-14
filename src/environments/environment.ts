d90e8a4822ec660bd0d9bf04c429a50ab5391f08
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-14 13:51:48 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    // url_auth: 'https://proveerten-auth.onrender.com',
-    // url_logic: 'https://proveerten-business.onrender.com'
-    url_auth: 'http://localhost:5001',
-    url_logic: 'http://localhost:4001'
+    url_auth: 'https://proveerten-auth.onrender.com',
+    url_logic: 'https://proveerten-business.onrender.com'
 };
