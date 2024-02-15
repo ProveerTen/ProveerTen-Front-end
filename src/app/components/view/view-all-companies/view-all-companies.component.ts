2690ffcb9ecfb91143a7bf1e9a69134083385a60
sofii11108 <sofia35025@gmail.com>
2024-02-15 18:35:20 -0500

    vista all companies lista

--- src/app/components/view/view-all-companies/view-all-companies.component.ts
+++ src/app/components/view/view-all-companies/view-all-companies.component.ts
@@ -13,6 +13,9 @@ export class ViewAllCompaniesComponent {
 
   id!: string;
   companies: any;
+  description:any;
+  showModal:any = false;
+  companyInfo:any;
 
   constructor(private client: ClientService, public auth: AuthService, private router: Router, private routerActivate: ActivatedRoute) { }
 
@@ -24,8 +27,12 @@ export class ViewAllCompaniesComponent {
       next: (response: any) => {
         this.companies = response.categoriesByCompanies;
 
+        this.companies.forEach(company => {
+          company.showMore = false 
+        });
+
         console.log(response.categoriesByCompanies);
-        console.log(response);        
+        console.log(this.companies);        
       },
       error: (error) => {
         console.log(error.error);
@@ -36,12 +43,34 @@ export class ViewAllCompaniesComponent {
   }
 
   viewProfile(id: string) {
+    event.stopPropagation();
     this.router.navigate(['profile/company', id])
+  }
+
+  showModalInfo(id: number) {
+    this.showModal = true
+    this.companyInfo = this.companies[id]
+    console.log("compa info", this.companyInfo);
+    
+    // this.router.navigate(['profile/company', id])
+  }
+  hideModalInfo() {
+    this.showModal = false
   }
 
   toggleShowMore(company: any) {
     console.log("toggle show more");
     
     company.showMore = !company.showMore;
+    event.stopPropagation();
+    // if (company.showMore) {
+    //   // Si showMore es true, muestra el mensaje completo
+    //   company.description_company = company?.description_company;
+    // } else {
+    //   // Si showMore es false, muestra solo los primeros 20 caracteres
+    //   company?.description_company.slice(0, 20) + '...';
+    // }
   }
+    // company.description_company = company.description_company.slice(0,21)
+  // }
 }
