07acda004a2d0f71b93f8db4de16bc77916e5002
Juan Camilo Quintero Jimenez <110494864+Bryctan@users.noreply.github.com>
2024-02-27 12:35:47 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
-export const environment = {
+export const environment_dev = {
     url_auth: 'http://localhost:5001',
     url_logic: 'http://localhost:4001',
     url_chat : 'http://localhost:3000'
-
 };
