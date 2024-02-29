e0dec34c6d76729ec0da1db5e8e46ccd2c74f36f
sofii11108 <sofia35025@gmail.com>
2024-02-28 20:28:59 -0500

    arreglos en el componente companies, sujeto a cambios

--- src/app/components/view/view-all-companies/view-all-companies.component.ts
+++ src/app/components/view/view-all-companies/view-all-companies.component.ts
@@ -12,54 +12,39 @@ import { SharedService } from '../../../services/shared/shared.service';
 })
 export class ViewAllCompaniesComponent {
 
-  id!: string;
   companies: any;
-  description:any;
-  showModal:any = false;
-  companyInfo:any;
   value: any;
   data: any;
 
   constructor(private client: ClientService, public auth: AuthService, private router: Router, private routerActivate: ActivatedRoute, private shared: SharedService) { }
 
-  ngOnInit(): void {
-    console.log('Hola');
+  ngOnInit() {
+    this.shared.valueRoute.subscribe(value => {
+      this.value = value;
+      if (this.value != null) {
+        this.getCompaniesByName();
+        return;
+      }
+    })
+    this.getCompanies();
+  }
 
-    // this.id = this.routerActivate.snapshot.params['id'];
-    this.client.getRequest(`${environment.url_logic}/view/companies`, undefined, undefined).subscribe({
+  getCompanies() {
+    this.client.getRequest(`${environment.url_logic}/profile/allCompaniesUserCero`, undefined, undefined).subscribe({
       next: (response: any) => {
-        this.companies = response.categoriesByCompanies;
-
-        this.companies.forEach(company => {
-          company.showMore = false 
-        });
-
-        console.log(response.categoriesByCompanies);
-        console.log(this.companies);        
+        this.data = response.data;
+        if (this.data.length == 0) {
+          console.log('No hay compañías por mostrar');
+        }
       },
       error: (error) => {
-        console.log(error.error);
-        // this.router.navigate(['404']);
+        console.log(error.error.Status);
       },
       complete: () => console.log('complete'),
     });
   }
 
-  viewProfile(id: string) {
-    event.stopPropagation();
-    this.router.navigate(['profile/company', id])
-  }
-
-  showModalInfo(id: number) {
-    this.showModal = true
-    this.companyInfo = this.companies[id]
-    console.log("compa info", this.companyInfo);
-    
-    // this.router.navigate(['profile/company', id])
-  }
-  hideModalInfo() {
-    this.showModal = false
-  // getCompaniesByName() {
+  getCompaniesByName() {
     this.client.postRequest(`${environment.url_logic}/search/companies/value`, { value: this.value }, undefined, undefined).subscribe({
       next: (response: any) => {
         this.data = response.values;
@@ -73,25 +58,8 @@ export class ViewAllCompaniesComponent {
       complete: () => console.log('complete'),
     });
   }
-  
 
-  toggleShowMore(company: any) {
-    console.log("toggle show more");
-    
-    company.showMore = !company.showMore;
-    event.stopPropagation();
-    // if (company.showMore) {
-    //   // Si showMore es true, muestra el mensaje completo
-    //   company.description_company = company?.description_company;
-    // } else {
-    //   // Si showMore es false, muestra solo los primeros 20 caracteres
-    //   company?.description_company.slice(0, 20) + '...';
-    // }
-  }
-    // company.description_company = company.description_company.slice(0,21)
-  // }
   viewCompany(id: string) {
     this.router.navigate(['profile/company/', id]);
   }
-// }
 }
