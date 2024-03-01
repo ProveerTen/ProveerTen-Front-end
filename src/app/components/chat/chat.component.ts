76aaeb2872d076b7ad8028c0c7c5eb8f91068e20
Jhon Herrera <jjhonherrera113@gmail.com>
2024-03-01 00:41:44 -0500

    Update chat scroll

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,22 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ChatService } from 'src/app/services/chat/chat.service';
 import { ClientService } from 'src/app/services/client/client.service';
 import { environment } from 'src/environments/environment';
-
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
-
+export class ChatComponent implements OnInit {
   @Input() chatId: string;
   @Input() value: number;
   messages: any[] = [];
   messageText: string = '';
   chats: string[] = [];
+  @ViewChild('messageContainer') messageContainer: ElementRef;
 
   constructor(private chatService: ChatService, private auth: AuthService, private client: ClientService) { }
 
@@ -29,11 +28,12 @@ export class ChatComponent {
 
     this.loadMessages();
     console.log(this.chatId);
-    this.chatService.joinChat(this.auth.getId(), this.chatId)
+    this.chatService.joinChat(this.auth.getId(), this.chatId);
     this.chatService.getMessages(this.chatId).subscribe(
       (message: any) => {
         console.log(message);
         this.messages.push(message);
+        this.scrollToBottom();
       }
     );
   }
@@ -43,11 +43,12 @@ export class ChatComponent {
       next: (response: any) => {
         console.log(response.chat.messages);
         this.messages = response.chat.messages;
+        this.scrollToBottom();
       },
       error: (error) => {
         console.log(error);
       }
-    })
+    });
   }
 
   sendMessage(): void {
@@ -56,8 +57,9 @@ export class ChatComponent {
         sender: this.auth.getId(),
         content: this.messageText
       };
-      this.chatService.sendMessage(messageData, this.chatId)
+      this.chatService.sendMessage(messageData, this.chatId);
       this.messageText = '';
+      this.scrollToBottom();
     }
   }
 
@@ -68,4 +70,10 @@ export class ChatComponent {
   ngOnDestroy() {
     this.messages = [];
   }
-} 
+
+  private scrollToBottom(): void {
+    setTimeout(() => {
+      this.messageContainer.nativeElement.scrollTop = this.messageContainer.nativeElement.scrollHeight;
+    });
+  }
+}
