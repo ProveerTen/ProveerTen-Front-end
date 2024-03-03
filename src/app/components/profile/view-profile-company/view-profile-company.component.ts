a54d5e733feea519c756e420d9ff66e6c082152b
sofii11108 <sofia35025@gmail.com>
2024-03-03 02:17:09 -0500

    cambios para a logica de redes sociales

--- src/app/components/profile/view-profile-company/view-profile-company.component.ts
+++ src/app/components/profile/view-profile-company/view-profile-company.component.ts
@@ -25,6 +25,8 @@ export class ViewProfileCompanyComponent {
   messages: any[] = [];
   showChat: boolean = false;
   chats: string[] = [];
+  dataSocialReds:any
+  colorsOfIcons: any[] = [];
 
   constructor(private client: ClientService, public auth: AuthService, private router: Router, private routerActivate: ActivatedRoute, private chatService: ChatService) {
     this.auth.isLoggedIn().subscribe((value: any) => {
@@ -46,6 +48,7 @@ export class ViewProfileCompanyComponent {
           const foundationDate = new Date(this.data.foundation_company);
           this.data.foundation_company = foundationDate.toISOString().split('T')[0];
           this.getPublications();
+          this.getSocialReds()
         },
         error: (error) => {
           console.log(error.error.Status);
@@ -79,6 +82,57 @@ export class ViewProfileCompanyComponent {
         complete: () => console.log('complete'),
       });
     }
+  }
+
+  getSocialReds() {
+    this.client.getRequest(`${environment.url_logic}/edit_profile/socialRed/${this.id}`, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
+      next: (response: any) => {
+        this.dataSocialReds = response.status.data;
+        console.log("social reds", this.dataSocialReds);            
+      },
+      error: (error) => {
+        console.log(error.error.Status);
+        this.router.navigate(['404']);
+      },
+      complete: () => {
+        console.log("Complete");   
+        this.getColorIcon()     
+      }
+    })
+  }
+
+  OptionsIcons = [
+    { value: '<i class="fa-brands fa-facebook"></i>', color: "color:#1a6eff;" },
+    { value: '<i class="fa-brands fa-instagram"></i>', color: "color:#fe2a90" },
+    { value: '<i class="fa-brands fa-youtube"></i>', color: "color:#ff1414" },
+    { value: '<i class="fa-brands fa-whatsapp"></i>', color: "color:#40a923" },
+    { value: '<i class="fa-brands fa-twitch"></i>', color: "color:#b62eff" },
+    { value: '<i class="fa-brands fa-discord"></i>', color: "color:#5863fe" },
+    { value: '<i class="fa-brands fa-twitter"></i>', color: "color:#2ec0ff" },
+    { value: '<i class="fa-brands fa-tiktok"></i>', color: "color:#000000" },
+    { value: '<i class="fa-brands fa-google-plus-g"></i>', color: "color:#ff2414" },
+    { value: '<i class="fa-solid fa-globe"></i>', color: "color:#e6a800" },
+    { value: '<i class="fa-solid fa-location-dot"></i>', color: "color:#b30000" },
+    { value: '<i class="fa-solid fa-map-location-dot"></i>', color: "color:#983c25"},
+    { value: '<i class="fa-solid fa-compact-disc"></i>', color: "color:#2a3c5a" }       
+  ];
+
+  getColorIcon() {
+    this.dataSocialReds.forEach(element => {
+      let etiqueteIcon = element.icon
+      // console.log("element",etiqueteIcon);
+
+      this.OptionsIcons.forEach(icon => {
+        let value = icon.value
+
+        if (etiqueteIcon == value) {
+          // this.colorIcon = icon.color
+          this.colorsOfIcons.push(icon.color)
+        }
+      })
+
+    });
+    console.log(this.colorsOfIcons);
   }
 
   getPublications() {
--- src/app/components/profile/view-profile/view-profile.component.ts
+++ src/app/components/profile/view-profile/view-profile.component.ts
@@ -19,10 +19,16 @@ export class ViewProfileComponent {
   cover: boolean = false;
   photo: boolean = false;
   formData = new FormData();
-  inputSocialRed: boolean = false
-  valueSocialRed: string
-  dataSocialReds:any
-  valid:boolean = false;
+
+  inputLinkRed: boolean = true
+  valueSocialRed: string = ""
+  dataSocialReds: any
+  valid: boolean = false;
+  iconSocialRed: any
+  estadoModalIcons: boolean = true
+  colorIcon: any;
+  colorsOfIcons: any[] = [];
+  // arrow = false
 
   constructor(private client: ClientService, public auth: AuthService, private router: Router, private messageService: MessageService) { }
 
@@ -46,7 +52,7 @@ export class ViewProfileComponent {
     });
   }
 
-  
+
   getPublications() {
     this.client.getRequest(`${environment.url_logic}/publication/view/company/${this.auth.getId()}`, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
       next: (response: any) => {
@@ -175,20 +181,30 @@ export class ViewProfileComponent {
       complete: () => console.log('complete'),
     });
   }
-
+  // 
+  // updateSocialRed() {
+  //   this.addSocialRed()
+  //   this.inputSocialRed = false
+  // }
 
   addSocialRed() {
-    this.inputSocialRed = !this.inputSocialRed
+    this.inputLinkRed = !this.inputLinkRed
+    this.estadoModalIcons = true
+    this.iconSocialRed = false
   }
+
   onSubmitSocialRed() {
+    if (this.valid == false) {
+      return
+    } else if (this.valid == true) {
     const data = {
       nit_company: this.auth.getId(),
       link: this.valueSocialRed,
-      icon: null
+      icon: this.iconSocialRed
     }
-    console.log(data);
+    console.log("data red", data);
 
-    this.client.postRequest(`${environment.url_logic}/edit_profile/company`, data, undefined, { "Authorization": `Bearer: ${this.auth.getToken()}` }).subscribe({
+    this.client.postRequest(`${environment.url_logic}/edit_profile/socialRed`, data, undefined, { "Authorization": `Bearer: ${this.auth.getToken()}` }).subscribe({
       next: (response: any) => {
         console.log(response);
         this.addSocialRed()
@@ -200,34 +216,132 @@ export class ViewProfileComponent {
         console.log("Complete");
         this.getSocialReds()
         this.valueSocialRed = null
+        this.iconSocialRed = null
+        this.estadoModalIcons = false
+        this.valid = false
       }
     })
+  }
   }
 
   valueRed(event: any) {
     this.valueSocialRed = event.target.value
-    if (this.valueSocialRed.length > 0) {
-      this.valid = true;      
-    } else {
-      this.valid = false;
-    }
+    // if (this.valueSocialRed.length > 2) {
+    //   this.valid = true;      
+    // } else {
+    //   this.valid = false;
+    // }
     console.log("VALID", this.valid);
-    
+
     console.log("EVENT", this.valueSocialRed);
+    this.validOnSubmit()
+
+
   }
 
+  validOnSubmit() {
+    console.log(this.valueSocialRed.length > 4  );
+    console.log(this.iconSocialRed );
+    
+    // let valid = false
+    if (this.iconSocialRed && this.valueSocialRed.length > 4) {
+      this.valid = true
+
+    } else {
+      this.valid = false
+    }
+    console.log("VAlID", this.valid);
+    return this.valid
+
+    // this.valid = valid
+  }
+  iconRed(valueIcon: any, colorIcon: any) {
+    // this.iconSocialRed = event.target.value
+    // console.log("iconSocialRed", this.iconSocialRed);
+    this.iconSocialRed = valueIcon
+    console.log("valueIcon", this.iconSocialRed);
+
+    this.colorIcon = colorIcon
+    console.log("index", this.colorIcon);
+    this.validOnSubmit()
+
+  }
+
+
+  OptionsIcons = [
+    { value: '<i class="fa-brands fa-facebook"></i>', color: "color:#1a6eff;" },
+    { value: '<i class="fa-brands fa-instagram"></i>', color: "color:#fe2a90" },
+    { value: '<i class="fa-brands fa-youtube"></i>', color: "color:#ff1414" },
+    { value: '<i class="fa-brands fa-whatsapp"></i>', color: "color:#40a923" },
+    { value: '<i class="fa-brands fa-twitch"></i>', color: "color:#b62eff" },
+    { value: '<i class="fa-brands fa-discord"></i>', color: "color:#5863fe" },
+    { value: '<i class="fa-brands fa-twitter"></i>', color: "color:#2ec0ff" },
+    { value: '<i class="fa-brands fa-tiktok"></i>', color: "color:#000000" },
+    { value: '<i class="fa-brands fa-google-plus-g"></i>', color: "color:#ff2414" },
+    { value: '<i class="fa-solid fa-globe"></i>', color: "color:#e6a800" },
+    { value: '<i class="fa-solid fa-location-dot"></i>', color: "color:#b30000" },
+    { value: '<i class="fa-solid fa-map-location-dot"></i>', color: "color:#983c25"},
+    { value: '<i class="fa-solid fa-compact-disc"></i>', color: "color:#2a3c5a" }       
+  ];
+
+  getColorIcon() {
+    this.dataSocialReds.forEach(element => {
+      let etiqueteIcon = element.icon
+      // console.log("element",etiqueteIcon);
+
+      this.OptionsIcons.forEach(icon => {
+        let value = icon.value
+
+        if (etiqueteIcon == value) {
+          // this.colorIcon = icon.color
+          this.colorsOfIcons.push(icon.color)
+        }
+      })
+
+    });
+    console.log(this.colorsOfIcons);
+  }
+  
+  showModalIcons() {
+    this.estadoModalIcons = !this.estadoModalIcons
+    // this.arrow = !this.arrow
+  }
   getSocialReds() {
 
-    this.client.getRequest(`${environment.url_logic}/edit_profile/company`, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
+    this.client.getRequest(`${environment.url_logic}/edit_profile/socialRed`, undefined, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
       next: (response: any) => {
-        this.dataSocialReds = response.status.data;   
-        console.log("datos de las redes sociales", this.dataSocialReds);                
+        this.dataSocialReds = response.status.data;
+        console.log("datos de las redes sociales", this.dataSocialReds);
+        console.log(this.dataSocialReds.length);
+        this.colorsOfIcons = []
+        this.getColorIcon()
       },
       error: (error) => {
         console.log(error.error.Status);
       },
       complete: () => {
         console.log('complete')
+      }
+    })
+  }
+
+  onDeleteSocialRed(id: string) {
+    // let id = id
+    console.log("id link", id);
+
+    this.client.deleteRequest(`${environment.url_logic}/edit_profile/socialRed`, { id }, { "Authorization": `Bearer ${this.auth.getToken()}` }).subscribe({
+      next: (response: any) => {
+        this.dataSocialReds = response.status.data;
+        console.log("datos de las redes sociales", this.dataSocialReds);
+        console.log(this.dataSocialReds.length);
+
+      },
+      error: (error) => {
+        console.log(error.error.Status);
+      },
+      complete: () => {
+        console.log('complete')
+        this.getSocialReds()
       }
     })
   }
