9352892fbcff44cd43e387704e870d16a7840a5f
Jhon Stiven Arcila Herrera <110495088+JhoonHe@users.noreply.github.com>
2024-04-05 02:08:13 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,5 @@
 export const environment = {
-    url_auth: 'https://proveerten-auth.onrender.com',
-    url_logic: 'https://proveerten-business.onrender.com',
-    url_chat: 'https://proveerten-chat-services.onrender.com',
-    //url_auth: 'http://localhost:5001',
-    //url_logic: 'http://localhost:4001',
-    //url_chat: 'http://localhost:3000' 
+    url_auth: 'https://auth-proveerten.azurewebsites.net',
+    url_logic: 'https://logic-proveerten.azurewebsites.net',
+    url_chat: 'https://chat-proveerten.azurewebsites.net'
 };
